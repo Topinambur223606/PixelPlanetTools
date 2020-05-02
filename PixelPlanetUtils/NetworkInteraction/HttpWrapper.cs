9904f5ed18ae5c355f12098d220cf30c121c3402
10 корень из 5 <gd22.3606@gmail.com>
2020-05-02 21:03:09 +0300

    updated user agent to latest chrome value

--- PixelPlanetUtils/NetworkInteraction/HttpWrapper.cs
+++ PixelPlanetUtils/NetworkInteraction/HttpWrapper.cs
@@ -160,7 +160,7 @@ namespace PixelPlanetUtils.NetworkInteraction
             request.Timeout = timeout;
             request.Proxy = Proxy;
             request.Headers["Origin"] = request.Referer = UrlManager.BaseHttpAdress;
-            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36";
             if (data != null)
             {
                 request.Method = "POST";
