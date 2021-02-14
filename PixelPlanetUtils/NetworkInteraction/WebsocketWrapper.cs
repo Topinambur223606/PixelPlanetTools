20a2574e782d3df279dbe05aa5a161a957eea611
10 корень из 5 <gd22.3606@gmail.com>
2021-02-14 15:13:03 +0200

    project version changes, changing websocket library dependency: local build instead of package

--- PixelPlanetUtils/NetworkInteraction/WebsocketWrapper.cs
+++ PixelPlanetUtils/NetworkInteraction/WebsocketWrapper.cs
@@ -45,6 +45,8 @@ namespace PixelPlanetUtils.NetworkInteraction
             }
             this.logger = logger;
             webSocket = new WebSocket(UrlManager.WebSocketUrl);
+            webSocket.Origin = "https://pixelplanet.fun";
+            webSocket.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36";
             webSocket.Log.Output = LogWebsocketOutput;
             if (proxySettings != null)
             {
